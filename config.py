7418389ca72c9a9b0c2db5a273ff5b77d86f2904
Hasstrup <31807665+Hasstrup@users.noreply.github.com>
2017-10-01 02:22:53 +0100

    first commit

---
+++ config.py
@@ -0,0 +1,33 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+WHOOSH_BASE = os.path.join(basedir, 'search.db')
+
+CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+# mail server settings
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = os.environ.get('hasstrup.ezekiel@gmail.com')
+MAIL_PASSWORD = os.environ.get('Onosetale32')
+
+# administrator list
+ADMINS = ['hasstrup.ezekiel@gmail.com', 'hasstrup.ezekiel@yahoo.com']
+
+# pagination
+POSTS_PER_PAGE = 3
+
+#Maxixmum search
+MAX_SEARCH_RESULTS = 50
